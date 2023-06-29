33cf6e38cbc7835eb508088016a0bd49795f73e2
Jean Demel <jean@formateur.fr>
2023-06-29 12:33:50 +0200

    calcul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BoucleComponent } from './boucle/boucle.component';
 import { CounterComponent } from './counter/counter.component';
 import { FormsModule } from '@angular/forms';
+import { CalculComponent } from './calcul/calcul.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoucleComponent,
-    CounterComponent
+    CounterComponent,
+    CalculComponent
   ],
   imports: [
     BrowserModule,
